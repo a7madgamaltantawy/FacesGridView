c27ae88ada4b84db594df56fcd08e8aad17027e2
ahmed gamal <a7mad_gamal@live.com>
2021-01-14 20:43:21 +0200

    Initial Commit

---
+++ FacesGridView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacesGridView
+//
+//  Created by ahmed tantawy on 14/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
